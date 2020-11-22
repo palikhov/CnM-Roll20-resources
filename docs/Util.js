3d266bf5417f535faa4dabaaf0414b064c7aaa5a
TheGiddyLimit <TheGiddyLimit@users.noreply.github.com>
2020-11-22 14:32:22 +0000

    Add thumbnail size slider

--- docs/Util.js
+++ docs/Util.js
@@ -35,7 +35,7 @@ window.$$ = function (parts, ...args) {
 		const handleArg = (arg) => {
 			if (arg instanceof $) {
 				$eles.push(arg);
-				return `<${arg.tag()} data-r="true"/>`;
+				return `<${arg.tag()} data-r="true"></${arg.tag()}>`;
 			} else if (arg instanceof HTMLElement) {
 				return handleArg($(arg));
 			} else return arg
@@ -54,7 +54,7 @@ window.$$ = function (parts, ...args) {
 			else $res.find(`[data-r=true]`).replaceWith(i => $eles[i]);
 		} else {
 			// Handle case where user has passed in a bunch of elements with no outer wrapper
-			const $tmp = $(`<div/>`);
+			const $tmp = $(`<div></div>`);
 			$tmp.append($res);
 			$tmp.find(`[data-r=true]`).replaceWith(i => $eles[i]);
 			return $tmp.children();
@@ -142,6 +142,156 @@ class MiscUtil {
 	static pDelay (msecs, resolveAs) {
 		return new Promise(resolve => setTimeout(() => resolve(resolveAs), msecs));
 	}
+
+	/**
+	 * Borrowed from lodash.
+	 *
+	 * @param func The function to debounce.
+	 * @param wait Minimum duration between calls.
+	 * @param options Options object.
+	 * @return {Function} The debounced function.
+	 */
+	static debounce (func, wait, options) {
+		let lastArgs; let lastThis; let maxWait; let result; let timerId; let lastCallTime; let lastInvokeTime = 0; let leading = false; let maxing = false; let trailing = true;
+
+		wait = Number(wait) || 0;
+		if (typeof options === "object") {
+			leading = !!options.leading;
+			maxing = "maxWait" in options;
+			maxWait = maxing ? Math.max(Number(options.maxWait) || 0, wait) : maxWait;
+			trailing = "trailing" in options ? !!options.trailing : trailing;
+		}
+
+		function invokeFunc (time) {
+			let args = lastArgs; let thisArg = lastThis;
+
+			lastArgs = lastThis = undefined;
+			lastInvokeTime = time;
+			result = func.apply(thisArg, args);
+			return result;
+		}
+
+		function leadingEdge (time) {
+			lastInvokeTime = time;
+			timerId = setTimeout(timerExpired, wait);
+			return leading ? invokeFunc(time) : result;
+		}
+
+		function remainingWait (time) {
+			let timeSinceLastCall = time - lastCallTime; let timeSinceLastInvoke = time - lastInvokeTime; let result = wait - timeSinceLastCall;
+			return maxing ? Math.min(result, maxWait - timeSinceLastInvoke) : result;
+		}
+
+		function shouldInvoke (time) {
+			let timeSinceLastCall = time - lastCallTime; let timeSinceLastInvoke = time - lastInvokeTime;
+
+			return (lastCallTime === undefined || (timeSinceLastCall >= wait) || (timeSinceLastCall < 0) || (maxing && timeSinceLastInvoke >= maxWait));
+		}
+
+		function timerExpired () {
+			const time = Date.now();
+			if (shouldInvoke(time)) {
+				return trailingEdge(time);
+			}
+			// Restart the timer.
+			timerId = setTimeout(timerExpired, remainingWait(time));
+		}
+
+		function trailingEdge (time) {
+			timerId = undefined;
+
+			if (trailing && lastArgs) return invokeFunc(time);
+			lastArgs = lastThis = undefined;
+			return result;
+		}
+
+		function cancel () {
+			if (timerId !== undefined) clearTimeout(timerId);
+			lastInvokeTime = 0;
+			lastArgs = lastCallTime = lastThis = timerId = undefined;
+		}
+
+		function flush () {
+			return timerId === undefined ? result : trailingEdge(Date.now());
+		}
+
+		function debounced () {
+			let time = Date.now(); let isInvoking = shouldInvoke(time);
+			lastArgs = arguments;
+			lastThis = this;
+			lastCallTime = time;
+
+			if (isInvoking) {
+				if (timerId === undefined) return leadingEdge(lastCallTime);
+				if (maxing) {
+					// Handle invocations in a tight loop.
+					timerId = setTimeout(timerExpired, wait);
+					return invokeFunc(lastCallTime);
+				}
+			}
+			if (timerId === undefined) timerId = setTimeout(timerExpired, wait);
+			return result;
+		}
+
+		debounced.cancel = cancel;
+		debounced.flush = flush;
+		return debounced;
+	}
+
+	// from lodash
+	static throttle (func, wait, options) {
+		let leading = true; let trailing = true;
+
+		if (typeof options === "object") {
+			leading = "leading" in options ? !!options.leading : leading;
+			trailing = "trailing" in options ? !!options.trailing : trailing;
+		}
+
+		return this.debounce(func, wait, {leading, maxWait: wait, trailing});
+	}
 }
 
-export {SortUtil, MiscUtil};
+class StorageUtil {
+	static _getSyncStorage () {
+		if (StorageUtil._init) {
+			if (StorageUtil.__fakeStorage) return StorageUtil._fakeStorage;
+			else return window.localStorage;
+		}
+
+		StorageUtil._init = true;
+		try {
+			window.localStorage.setItem("_test_storage", true);
+			return window.localStorage;
+		} catch (e) {
+			// if the user has disabled cookies, build a fake version
+			StorageUtil.__fakeStorage = true;
+			StorageUtil._fakeStorage = {
+				isSyncFake: true,
+				getItem: k => StorageUtil.__fakeStorage[k],
+				removeItem: k => delete StorageUtil.__fakeStorage[k],
+				setItem: (k, v) => StorageUtil.__fakeStorage[k] = v,
+			};
+			return StorageUtil._fakeStorage;
+		}
+	}
+
+	// region Synchronous
+	static syncGet (key) {
+		const rawOut = StorageUtil._getSyncStorage().getItem(key);
+		if (rawOut && rawOut !== "undefined" && rawOut !== "null") return JSON.parse(rawOut);
+		return null;
+	}
+
+	static syncSet (key, value) {
+		StorageUtil._getSyncStorage().setItem(key, JSON.stringify(value));
+	}
+
+	static syncRemove (key) {
+		StorageUtil._getSyncStorage().removeItem(key);
+	}
+	// endregion
+}
+StorageUtil._init = false;
+StorageUtil._fakeStorage = {};
+
+export {SortUtil, MiscUtil, StorageUtil};
